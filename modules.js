2053b1f1cab5f7e11dcbd00c771fe8090c780dd3
alorca89 <albalorca@gmail.com>
2020-06-18 19:33:15 +0200

    afegim arxiu js

---
+++ modules.js
@@ -0,0 +1,8 @@
+function calculapreutotal (preu1, preu2){
+        var preutotal = preu1 + preu2;
+        return preutotal;
+    }
+    preu1 = 5.55;
+    preu2 = 3.35;
+    preutotal = calculapreutotal (preu1, preu2);
+    alert (preutotal+ " €");
